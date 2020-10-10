fa1395e9cdbb212fd7724cd809e61a88cfe2669c
Roger Rezende <roger.rezendegmail.com>
2020-10-10 14:34:54 -0300

    criação do projeto da API

---
+++ src/main/java/zup/bancodigitalapi/BancoDigitalApiApplication.java
@@ -0,0 +1,13 @@
+package zup.bancodigitalapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoDigitalApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoDigitalApiApplication.class, args);
+	}
+
+}
